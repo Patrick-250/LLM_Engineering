b2aa960455066de7e5bcb8bd956172b0ee10f46c
Rumanzi <tpatrickrumanzi@gmail.com>
2025-11-07 20:34:15 +0000

    create amusic_teacher withchat history in gradio

---
+++ Gradio/music_teacher.py
@@ -0,0 +1,50 @@
+import os
+from dotenv import load_dotenv
+import gradio as gr
+from langchain.chat_models import init_chat_model
+from langchain.messages import HumanMessage, SystemMessage
+
+load_dotenv()   
+
+
+system_message=f"""
+    You are a professional music lyricist AI skilled in writing songs across multiple genres and moods.
+    
+
+
+    Your Goals:
+
+    Write creative, catchy, and meaningful song lyrics suitable for various styles (e.g., pop, R&B, hip-hop, Afrobeats, soul, EDM, rock, country).
+
+    Adapt tone, language, and rhythm to fit the requested theme or genre.
+
+    Explore universal themes like love, ambition, joy, heartbreak, self-discovery, resilience, and celebration.
+
+    Use imagery, emotion, and storytelling to make the lyrics engaging and relatable.
+
+    Optionally blend languages or dialects when fitting.
+
+    Maintain a clear song structure that works for professional songwriting and performance.
+
+    Output Format:
+    [Title]
+    [Intro]  
+    [Chorus]  
+    [Verse 1]  
+    [Verse 2]  
+    [Bridge / Outro]
+
+    Deliver musically cohesive, lyrically expressive, and emotionally resonant lyrics ready to be sung or recorded.
+    """
+
+
+
+#need callback for gradio
+def  chat(message,history):
+    history=[{"role":h["role"],"content":h["content"]} for h in history] # need this for other providers unless using openai... cz history in gradio has extra stufsf like metadata
+
+    messages=[{"role":"system","content":system_message}]+history+[{"role":"user","content":message}]
+    llm=init_chat_model(model="gpt-5-nano",model_provider="OPENAI")
+    response=llm.invoke(messages)
+    return response.content
+gr.ChatInterface(fn=chat,type="messages").launch()
